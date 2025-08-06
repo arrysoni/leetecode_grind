6b9a4d7b76508bfbe9a4c6804293ed9ea606ee83
Aarya Soni <aarya19204@gmail.com>
2025-08-06 22:50:59 +0530

    Fixed Roman Numerals

--- romanToInt.py
+++ romanToInt.py
@@ -8,24 +8,21 @@ class Solution(object):
         roman_nums = {'I': 1, 'V': 5, 'X': 10,
                       'L': 50, 'C':  100, 'D':  500, 'M':  1000}
 
-        reversed_string = s[::-1]
-        sum = 0
+        total = 0
+        prev_value = 0
 
-        for left in range(0, len(s)-1):
-            left_letter = reversed_string[left]
-            print("left_letter: ",left_letter)
-            for right in range(1, len(s)):
-                right_letter = reversed_string[right]
-                print("right_letter: ",right_letter)
-                print("roman_nums[left_letter]:", roman_nums[left_letter])
-                print("roman_nums[right_letter]", roman_nums[right_letter])
-                if (roman_nums[left_letter] >= roman_nums[right_letter]):
-                    sum = roman_nums[left_letter] + roman_nums[right_letter]
-                else:
-                    sum = roman_nums[left_letter] - roman_nums[right_letter]
+        # Traverse the string from right to left
+        for char in reversed(s):
+            value = roman_nums[char]
+            if value < prev_value:
+                total -= value
+            else:
+                total += value
+            prev_value = value
 
-        print(sum)
-        return sum
+        return total
 
 
-Solution().romanToInt("III")
+print(Solution().romanToInt("III"))  # Output: 3
+print(Solution().romanToInt("LVIII"))  # Output: 58
+print(Solution().romanToInt("MCMXCIV"))  # Output: 1994
